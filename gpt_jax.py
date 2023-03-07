efa60f263f7629861a41784cdc94c12fa75296c9
Aman K <opensourceaman@gmail.com>
2023-03-07 17:55:12 +0530

    refactor code, add loss estimate, code still diverging

--- gpt_jax.py
+++ gpt_jax.py
@@ -1,10 +1,10 @@
 import csv
 import logging
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional, Tuple, Callable, Union, Any
 
 import jax
 import jax.numpy as jnp
-from jax import lax, pmap
+from jax import lax, jit
 
 import numpy as np
 import optax
@@ -24,7 +24,7 @@ from jax_smi import initialise_tracking
 from mingpt.train import GraphemeVocab
 
 MAX_FIELD_SIZE = 100000000  # 100 MB
-EXPERIMENT_LABEL = "test_run_2"
+EXPERIMENT_LABEL = "test_run_3"
 
 # Set up logger
 logger = logging.getLogger(__name__)
@@ -193,7 +193,9 @@ class GPTLanguageModel(nn.Module):
 
 def generate(model: nn.Module, params: FrozenVariableDict, idx: jnp.ndarray, max_new_tokens: int):
     # idx is (B, T) array of indices in the current context
-    for _ in range(max_new_tokens):
+    key = jax.random.PRNGKey(4223)
+
+    for i in range(max_new_tokens):
         idx_cond = idx[:, -block_size:]
 
         # get the predictions
@@ -204,26 +206,38 @@ def generate(model: nn.Module, params: FrozenVariableDict, idx: jnp.ndarray, max
 
         # apply softmax to get probabilities
         probs = jax.nn.softmax(logits, axis=-1)  # (B, C)
-        probs = np.asarray(probs)
+        # probs = np.asarray(probs)
 
         # sample from the distribution
-        idx_next = np.random.choice(range(probs.shape[-1]), p=probs.ravel())
-        idx_next = rearrange(jnp.array(idx_next), "->1 1")
+        sample_key = jax.random.fold_in(key, i)
+        idx_next = jax.random.choice(sample_key, probs.shape[-1], shape=(1, 1), p=jnp.reshape(probs, (-1)))
 
         # append sampled index to the running sequence
         idx = jnp.concatenate([idx, idx_next], axis=1)  # (B, T+1)
     return idx
 
 
-def loss_fn(logits, targets):
-    if targets is None:
-        loss = None
-    else:
+def estimate_loss(data_dict, model: GPTLanguageModel, params, dropout_rng, eval_iters):
+    def loss_fn(model: GPTLanguageModel, params, inputs, labels):
+        logits = model.apply(params, inputs, rngs={"dropout": dropout_rng})
         B, T, C = logits.shape
+        labels = labels.reshape(B * T)
         logits = logits.reshape(B * T, C)
-        targets = targets.reshape(B * T)
-        loss = optax.softmax_cross_entropy_with_integer_labels(logits, targets)
-    return jnp.mean(loss)
+
+        loss = optax.softmax_cross_entropy_with_integer_labels(logits, labels)
+        return jnp.mean(loss)
+
+    out = {}
+    model.deterministic = True
+    for split in ["train", "validation"]:
+        losses = np.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split, data_dict, block_size, batch_size, for_pmap=False)
+            loss = loss_fn(model, params, X, Y)
+            losses[k] = loss
+        out[split] = losses.mean()
+    model.deterministic = False
+    return out
 
 
 def update(state: TrainState, inputs, labels, seed: int = 100):
@@ -279,45 +293,92 @@ def get_batch(split, data: Dict[str, torch.tensor], block_size: int, batch_size:
     return x, y
 
 
+def get_data_dict(text: str, encode: Callable) -> Dict[str, torch.tensor]:
+    # Train and test splits
+    data = torch.tensor(encode(text), dtype=torch.long)
+    n = int(0.9 * len(data))  # first 90% will be train, rest val
+    train_data = data[:n]
+    val_data = data[n:]
+    data_dict = {"train": train_data, "validation": val_data}
+
+    return data_dict
+
+
+def get_encoder_decoder(vocab: GraphemeVocab) -> Tuple[Callable, Callable]:
+    # create a mapping from characters to integers
+    stoi = vocab.stoi
+    itos = vocab.itos
+    encode = lambda s: [stoi[c] for c in Shabdansh(s)]  # encoder: take a string, output a list of integers
+    decode = lambda l: "".join([itos[i] for i in l])  # decoder: take a list of integers, output a string
+
+    return encode, decode
+
+
+def get_model_n_params(
+    vocab_size: int,
+    n_embd: int,
+    block_size: int,
+    n_layer: int,
+    n_head: int,
+    dropout: float,
+    data_dict: Dict[str, torch.tensor],
+    batch_size: int,
+    for_validation: bool = False,
+) -> Union[Tuple[GPTLanguageModel, FrozenVariableDict, Any], GPTLanguageModel]:
+    # prepare rngs
+    rng = jax.random.PRNGKey(0)
+    params_rng, dropout_rng = jax.random.split(rng)
+    rngs = {"params": params_rng, "dropout": dropout_rng}
+
+    # disable dropout when for validation
+    deterministic = for_validation
+
+    # create model
+    gpt = GPTLanguageModel(vocab_size, n_embd, block_size, n_layer, n_head, dropout, deterministic)
+
+    if not for_validation:
+        # get params
+        x, _ = get_batch("train", data_dict, block_size, batch_size)
+        params = gpt.init(rngs, x)
+
+        return gpt, params, dropout_rng
+
+    else:
+        return gpt
+
+
 # hyperparameters
-batch_size = 128  # how many independent sequences will we process in parallel?
-block_size = 512  # what is the maximum context length for predictions?
-max_iters = 50000
+batch_size = 64
+block_size = 256
+max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
-eval_iters = 200
+eval_iters = 20
 n_embd = 384
 n_head = 6
-n_layer = 6
+n_layer = 12
 dropout = 0.2
 
 text, vocab = load_data("/home/khandelia1000/premchand/data/premchand.tsv")
-
-# here are all the unique characters that occur in this text
-
 vocab_size = len(vocab.stoi)
-# create a mapping from characters to integers
-stoi = vocab.stoi
-itos = vocab.itos
-encode = lambda s: [stoi[c] for c in Shabdansh(s)]  # encoder: take a string, output a list of integers
-decode = lambda l: "".join([itos[i] for i in l])  # decoder: take a list of integers, output a string
 
-# Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9 * len(data))  # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
-data_dict = {"train": train_data, "validation": val_data}
+encode, decode = get_encoder_decoder(vocab)
+
+data_dict = get_data_dict(text, encode)
 
 initialise_tracking()
 # Initialize the parameters of the module
-rng = jax.random.PRNGKey(0)
-params_rng, dropout_rng = jax.random.split(rng)
-rngs = {"params": params_rng, "dropout": dropout_rng}
-deterministic = False
-gpt = GPTLanguageModel(vocab_size, n_embd, block_size, n_layer, n_head, dropout, deterministic)
-x, _ = get_batch("train", data_dict, block_size, batch_size)
-params = gpt.init(rngs, x)
+gpt, params, dropout_rng = get_model_n_params(
+    vocab_size,
+    n_embd,
+    block_size,
+    n_layer,
+    n_head,
+    dropout,
+    data_dict,
+    batch_size,
+)
+max_new_tokens = 10
 
 parameter_count = sum(x.size for x in jax.tree_util.tree_leaves(params)) / 1e6
 logger.info(f"Number of parameters (in millions): {parameter_count}")
@@ -325,7 +386,6 @@ logger.info(f"Number of parameters (in millions): {parameter_count}")
 
 state = TrainState.create(apply_fn=gpt.apply, params=params, tx=optax.adamw(learning_rate))
 state = jax_utils.replicate(state)
-loss = float("inf")
 
 p_update = jax.pmap(update, axis_name="batch")
 for iter in range(max_iters):
@@ -337,18 +397,11 @@ for iter in range(max_iters):
 
     # every once in a while evaluate the loss on train and val sets
     if iter % eval_interval == 0 or iter == max_iters - 1:
+        logger.info(estimate_loss(data_dict, gpt, jax_utils.unreplicate(state).params, dropout_rng, eval_iters))
         logger.info(f"step {iter}: train loss {jnp.mean(loss):.4f}")
         context = jnp.zeros((1, 1), dtype=jnp.int32)
-        print(decode(generate(gpt, jax_utils.unreplicate(state).params, context, max_new_tokens=500)[0].tolist()))
-
-# if __name__ == "__main__":
-#     determinstic = False
-#     # Instantiate your Flax nn.Module
-#     head_size = n_embd // n_head
-#     module = Head(head_size, dropout, block_size, determinstic)
-
-#     input_shape = (2, 10)
-#     x = jnp.zeros(input_shape, dtype=jnp.int32)  # example input
-
-#     inputs = jnp.ones(input_shape, dtype=jnp.int32)
-#     outputs = gpt.apply(params, inputs, rngs={"dropout": dropout_rng})
+        logger.info(
+            decode(
+                generate(gpt, jax_utils.unreplicate(state).params, context, max_new_tokens=max_new_tokens)[0].tolist()
+            )
+        )
