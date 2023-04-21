d201460c2be4d52e42fa5aa236bd39adca0d961b
Aman K <opensourceaman@gmail.com>
2023-04-22 01:19:55 +0530

    make generate code faster, still have scope for improvement

--- data_utils.py
+++ data_utils.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 
-from einops import rearrange
+from jax import lax
 
 from flax.core.scope import FrozenVariableDict
 
@@ -10,31 +10,46 @@ from mingpt.config import ModelConfig
 
 
 def generate(
-    idx: jnp.ndarray, model: GPTLanguageModel, params: FrozenVariableDict, config: ModelConfig, max_new_tokens: int, dropout_rng
+    idx: jnp.ndarray,
+    model: GPTLanguageModel,
+    params: FrozenVariableDict,
+    config: ModelConfig,
+    max_new_tokens: int,
+    dropout_rng,
+    pad_id: int = 0,
 ):
     model.deterministic = True
-    # idx is (B, T) array of indices in the current context
     key = jax.random.PRNGKey(4223)
 
-    for i in range(max_new_tokens):
-        idx_cond = idx[:, -config.arch.block_size :]
+    # Pad the input idx to have the final shape
+    prompt_token_count = idx.shape[-1]
+    idx = jnp.pad(
+        idx, ((0, 0), (0, config.arch.block_size - prompt_token_count)), mode="constant", constant_values=pad_id
+    )
 
-        # get the predictions
-        logits = model.apply(params, idx_cond, rngs={"dropout": dropout_rng})
+    def cond_fn(loop_state):
+        _, _, counter = loop_state
+        return counter < max_new_tokens
 
-        # focus only on the last time step
-        logits = logits[:, -1, :]  # becomes (B, C)
+    def body_fn(loop_state):
+        idx, key, counter = loop_state
+
+        logits = model.apply(params, idx, rngs={"dropout": dropout_rng})
+        # focus only on the counter step
+        logits = logits[:, prompt_token_count + counter, :]  # becomes (B, C)
 
         # apply softmax to get probabilities
         probs = jax.nn.softmax(logits, axis=-1)  # (B, C)
-        # probs = np.asarray(probs)
 
         # sample from the distribution
-        sample_key = jax.random.fold_in(key, i)
+        sample_key = jax.random.fold_in(key, counter)
         idx_next = jax.random.choice(sample_key, probs.shape[-1], shape=(1, 1), p=jnp.reshape(probs, (-1)))
 
         # append sampled index to the running sequence
-        idx = jnp.concatenate([idx, idx_next], axis=1)  # (B, T+1)
+        idx = lax.dynamic_update_slice(idx, idx_next, (0, prompt_token_count + counter))
+        return idx, key, counter + 1
+
+    idx, _, _ = lax.while_loop(cond_fn, body_fn, (idx, key, 0))
 
     model.deterministic = False
     return idx
